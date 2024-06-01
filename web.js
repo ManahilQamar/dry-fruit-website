9265d6e43c5fbd44bb23138cb7cf09b886b25704
manhil qamar <manhilqamar@gmail.coom>
2024-06-02 00:07:29 +0500

    some changes in product

--- web.js
+++ web.js
@@ -42,7 +42,7 @@ Object.values(categories).forEach(product => {
   const span = document.createElement('span');
   span.textContent = product[1];
   const icon = document.createElement('i');
-  icon.classList.add('bx', 'bx-right-arrow-alt'); // This adds the right arrow icon from Boxicons
+  // icon.classList.add('bx', 'bx-right-arrow-alt'); // This adds the right arrow icon from Boxicons
 
 
   box.appendChild(img);
@@ -55,16 +55,62 @@ Object.values(categories).forEach(product => {
 
 
 
+// product............
+
+
+
+const products = {
+  1: ['Product 1', 'Product 1 is great', 'almand.jpg'],
+  2: ['Product 2', 'Product 2 is awesome', 'choco.jpeg'],
+  3: ['Product 3', 'Product 3 is fantastic', 'product3.jpg'],
+  4: ['Product 4', 'Product 4 is superb', 'product4.jpg'],
+  5: ['Product 5', 'Product 5 is excellent', 'product5.jpg'],
+};
+
+const productsContainer = document.querySelector('.products2-container');
+
+Object.values(products).forEach(product => {
+  const box = document.createElement('div');
+  box.classList.add('box');
+
+  const img = document.createElement('img');
+  img.src = product[2];
+  img.alt = product[0];
+
+  const h2 = document.createElement('h2');
+  h2.textContent = product[0];
+
+  const span = document.createElement('span');
+  span.textContent = product[1];
+  
+  const icon = document.createElement('i');
+  icon.classList.add('bx', 'bx-right-arrow-alt'); 
+  
+  const icon1 = document.createElement('i');
+  icon1.classList.add('bx', 'bx-heart'); 
+
+  const icon2 = document.createElement('i');
+  icon2.classList.add('bx', 'bx-cart-alt'); 
+
+  box.appendChild(img);
+  box.appendChild(h2);
+  box.appendChild(span);
+  box.appendChild(icon); 
+  box.appendChild(icon1); 
+  box.appendChild(icon2); 
+  productsContainer.appendChild(box);
+});
 
 
 
 //............ swiper.................//
-var swiper = new Swiper(".homes", {
-  spaceBetween: 25,
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
   centeredSlides: true,
+ 
+ 
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
-
