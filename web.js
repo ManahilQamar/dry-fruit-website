c2895e9be380cff9962419945e93e3092f994ed7
manhil qamar <manhilqamar@gmail.coom>
2024-06-09 01:29:26 +0500

    make search

--- web.js
+++ web.js
@@ -16,6 +16,14 @@ callback: cb
 })();
 
 
+$(document).ready(function() {
+  $("#myinput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $(".box").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+  });
+});
 
 
 
